75f19954ea4b84617b74189b2c902432447fee7b
RubenSH51 <rubencuello221b@gmail.com>
2025-01-26 18:59:01 -0300

    actualizando index.html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Frontendtest/',
+  base: "https://RubenSH51.github.io/Frontendtest",
   plugins: [react()],
 })
